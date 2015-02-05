31b9aabaf35ac1a4171874c1dc5b5d4dddab60b1
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-02-05 15:48:03 +0100

    Añadida paginacion con boostrap

--- static/scripts/controllers/search.js
+++ static/scripts/controllers/search.js
@@ -1,54 +1,73 @@
-'use strict';
+	'use strict';
 
-/**
- * @ngdoc function
- * @name pruebaApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the pruebaApp
- */
+	/**
+	 * @ngdoc function
+	 * @name pruebaApp.controller:AboutCtrl
+	 * @description
+	 * # AboutCtrl
+	 * Controller of the pruebaApp
+	 */
 
-angular.module('PolymerBricks')
-.controller('SearchCtrl', function ($scope) {
+	angular.module('PolymerBricks')
+	.controller('SearchCtrl', function ($scope) {
 
-	$scope.orderBy='asc';
-	$scope.sortBy='name';
-	$scope.webComponents = $scope.$parent.webComponents;
+		$scope.orderBy='asc';
+		$scope.sortBy='name';
+		$scope.webComponents = $scope.$parent.webComponents;
 
-	$scope.setSort = function (sortBy) {
-		$scope.sortBy=sortBy;
-		getAllComponent($scope.orderBy,$scope.sortBy);
-	};
+		$scope.setSort = function (sortBy) {
+			$scope.sortBy=sortBy;
+			getAllComponent($scope.orderBy,$scope.sortBy);
+		};
 
-	$scope.setOrder = function () {
-		if ($scope.orderBy ==='des'){
-			$scope.orderBy = 'asc';
-		} else {
-			$scope.orderBy = 'des';
-		}
-	};
-
-	$scope.changeList = function (sortBy) {
-		$scope.setOrder();
-		$scope.setSort(sortBy);
-	};
-	$scope.init = function (){
-		var callback = function(response) {
-
-			$scope.$apply(function() {
-				$scope.webComponents = response;
+		$scope.setOrder = function () {
+			if ($scope.orderBy ==='des'){
+				$scope.orderBy = 'asc';
+			} else {
+				$scope.orderBy = 'des';
 			}
-						 )};
-		getAllComponents($scope.orderBy,$scope.sortBy,callback);
-	};
+		};
 
-	$scope.respuesta = '';
-	$scope.open = function () {
-		var validInput = document.getElementById('inputC');
+		$scope.changeList = function (sortBy) {
+			$scope.setOrder();
+			$scope.setSort(sortBy);
+		};
 
-		if (validInput.validity.valid && validInput.inputValue!==null) {
+		$scope.init = function (){
+			var callback = function(response) {
 
-			google.appengine.api.polymerBricks.uploadComponent(validInput.inputValue);
-		}
-	};
-});
+				$scope.$apply(function() {
+					$scope.webComponents = response;
+					$scope.makeTodos();
+					$scope.$watch('currentPage + numPerPage', function() {
+						var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+						, end = begin + $scope.numPerPage;
+
+						$scope.filteredTodos =$scope.todos.slice(begin, end);
+					});
+
+
+				});
+			};
+			getAllComponents($scope.orderBy,$scope.sortBy,callback);
+		};
+
+		$scope.filteredTodos = []
+		,$scope.currentPage = 1
+		,$scope.numPerPage = 1
+		,$scope.maxSize = 3;
+
+		$scope.todos = [];
+		var i;
+		$scope.makeTodos = function() {
+			for (i=0;i<$scope.webComponents.length;i++) {
+				$scope.todos.push($scope.webComponents[i]);
+			}
+		};
+
+		$scope.numPages = function () {
+			return Math.ceil($scope.todos.length / $scope.numPerPage);
+		};
+
+
+	});
