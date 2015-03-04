89c9131eee8d22c7685e393157fba000c485e216
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-03-04 14:06:22 +0100

    Añadido popup de login

--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -11,31 +11,12 @@
 
 angular.module('PolymerBricks')
 .controller('MainCtrl', function ($scope,$location,$timeout) {
-
-
-	$scope.buscar=false;
-	$scope.changeView = function(view){
-		if (view === 'profile/'){
-			view=view + $scope.user.id.toString();
-		}
-		$location.path(view); // path not hash
-	};
-	$scope.logueado=false;
-	$scope.profile = 'hola';
-	$scope.user = $scope.user || [];
-	$scope.init = function (){
-		/*google.appengine.api.polymerBricks.getAllComponentsLimit('asc','name',3);*/
-	};
+	$scope.popup = false;
 	
-	$scope.showOn = function () {
-		 $timeout(function() {
-    			document.querySelector('#search').focus();
-			 		$scope.changeView('search');
-        });
-	};
-	$scope.showOff = function (e) {
-		if (e.target.value === '') {
-				$scope.buscar=false;	
-		}
-	};
+	$scope.showPopup = function(){
+		$scope.popup = true;
+	}
+	$scope.hidePopup = function(){
+		$scope.popup = false;
+	}
 });
