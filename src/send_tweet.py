a669f16cb926e3378a03300f96b475ce629f896a
lruizr <l.ruizr04@gmail.com>
2015-09-11 10:31:06 +0200

    No se envía el tweet

--- src/send_tweet.py
+++ src/send_tweet.py
@@ -4,7 +4,7 @@ import httplib, urllib
 #uri = "https://api.twitter.com/1.1/statuses/update.json"
 
 connection = httplib.HTTPSConnection("api.twitter.com")
-params = urllib.encode({
+params = urllib.urlencode({
 	"Authorization": "OAuth",
     "oauth_consumer_key": "DC0sePOBbQ8bYdC8r4Smg",
     "oauth_signature_method": "HMAC-SHA1",
@@ -12,15 +12,14 @@ params = urllib.encode({
 	"oauth_nonce": "-449675027",
 	"oauth_version": "1.0",
 	"oauth_token": "3072043347-zCS2VuxT724d4Uk6UoH675i0XCa0l9uaCLo6FeN",
-	"oauth_signature": "PlK3ohWkSH24a2MCPAO7qI6QMCU%3D",
-	"status": "Bienvenidos al twitter oficial del grupo DEUS"
+	"oauth_signature": "PlK3ohWkSH24a2MCPAO7qI6QMCU%3D"
 })
 
 headers = {"Content-type": "application/x-www-form-urlencoded",
 			"Accept": "text/plain"}
-connection.request("POST", "/1.1/statuses/update.json", params, headers)
+connection.request("POST", "/1.1/statuses/update.json?status=Bienvenidos al twitter oficial del grupo DEUS", params, headers)
 response = connection.getresponse()
 data = response.read()
+header = response.getheaders()
 connection.close()
-
-print data
+print header
