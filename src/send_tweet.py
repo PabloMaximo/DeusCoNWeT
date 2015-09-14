b4cda1b79e23717318f8e386e69785ec6725a9b8
lruizr <l.ruizr04@gmail.com>
2015-09-14 12:28:40 +0200

    Using Twython

--- src/send_tweet.py
+++ src/send_tweet.py
@@ -1,25 +1,27 @@
-from time import time
-import httplib, urllib
+from twython import Twython
 
 #uri = "https://api.twitter.com/1.1/statuses/update.json"
+twitter = Twython("J4bjMZmJ6hh7r0wlG9H90cgEe", "8HIPpQgL6d3WWQMDN5DPTHefjb5qfvTFg78j1RdZbR19uEPZMf",
+					"3072043347-hbcrkzLJfVzTg7BTjgzkKqZx3bbzpYb04IO573x", "VmQX0z3ZWpRv63M92z0SrmmUNGFjNIMZ06iGiJ67kK9oY")
+resp = twitter.update_status(status="Testing our API")
+print resp
+# connection = httplib.HTTPSConnection("api.twitter.com")
+# params = urllib.urlencode({
+# 	"Authorization": "OAuth",
+#     "oauth_consumer_key": "DC0sePOBbQ8bYdC8r4Smg",
+#     "oauth_signature_method": "HMAC-SHA1",
+#     "oauth_timestamp": str(int(time())),
+# 	"oauth_nonce": "-449675027",
+# 	"oauth_version": "1.0",
+# 	"oauth_token": "3072043347-zCS2VuxT724d4Uk6UoH675i0XCa0l9uaCLo6FeN",
+# 	"oauth_signature": "PlK3ohWkSH24a2MCPAO7qI6QMCU%3D"
+# })
 
-connection = httplib.HTTPSConnection("api.twitter.com")
-params = urllib.urlencode({
-	"Authorization": "OAuth",
-    "oauth_consumer_key": "DC0sePOBbQ8bYdC8r4Smg",
-    "oauth_signature_method": "HMAC-SHA1",
-    "oauth_timestamp": str(int(time())),
-	"oauth_nonce": "-449675027",
-	"oauth_version": "1.0",
-	"oauth_token": "3072043347-zCS2VuxT724d4Uk6UoH675i0XCa0l9uaCLo6FeN",
-	"oauth_signature": "PlK3ohWkSH24a2MCPAO7qI6QMCU%3D"
-})
-
-headers = {"Content-type": "application/x-www-form-urlencoded",
-			"Accept": "text/plain"}
-connection.request("POST", "/1.1/statuses/update.json?status=Bienvenidos al twitter oficial del grupo DEUS", params, headers)
-response = connection.getresponse()
-data = response.read()
-header = response.getheaders()
-connection.close()
-print header
+# headers = {"Content-type": "application/json",
+# 			"Accept": "text/json"}
+# connection.request("POST", "/1.1/statuses/update.json?status=Bienvenidos al twitter oficial del grupo DEUS", params, headers)
+# response = connection.getresponse()
+# data = response.read()
+# header = response.getheaders()
+# connection.close()
+# print data
